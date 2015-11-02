ea8147947c1b5ab79e91cb0d053569d40c319f10
thozh912 <thomaszhangswe@gmail.com>
2015-11-02 17:17:36 +0100

    OK done

--- R/tecatorassignment.R
+++ R/tecatorassignment.R
@@ -1,6 +1,6 @@
 library(XLConnect)
 library(glmnet)
-
+library(MASS)
 #hehehehehe
 #FROM THIS FILE LOCATION, EXCEL FILES SHOULD BE FOUND IN A SUBFOLDER IN THIS FILE LOCATION CALLED DATA
 wb = loadWorkbook("data/tecator.xlsx")
@@ -12,7 +12,7 @@ plot(data2$Protein,data2$Moisture,pch="+",main="Plot of Moisture against Protein
 # $y_{j} = \sum_{i=0}^{n} w_{i}x_{j}^{i} + e$ , $e$ is $N(0,\sigma^2)$ distributed.
 
 n=dim(data2)[1]
-set.seed(78)
+set.seed(12345)
 id=sample(1:n, floor(n*0.5))
 train=data2[id,]
 valid=data2[-id,]
@@ -64,8 +64,58 @@ legend("topright",c("training data","validation data"),
        lty=c(1,1),
        lwd=c(2.5,2.5),col=c("black","red"))
 
-#AIC = -2loglik(D) + 2d(model) where d(model) is the no. of parameters.
+#AIC = -2loglik(D) + 2d(model) where d(model) is the no. of parameters. sigma2 counts as a parameter
 # We make assumption that the outcomes are normally distributed where for observation j mean is 
 # \sum_{i=0}^{n} w_{i}x_{j}^{i} and variance is \sigma^2
 
-AIC_calc <- function()
+#We estimate \sigma^2 by the MSE of linear model of total data set.
+
+Xlinearforentire <- cbind(rep(1,n),data2$Protein)
+predlinearforentire <- Xlinearforentire %*% wvalues[[1]]
+sigma2 <- sum(((data2$Moisture) - predlinearforentire)^2) / n
+
+AIC_calc <- function(data2,wvalues,sigma2){
+  AICs <- c()
+  
+  for(j in 1:6){
+    jpt <- 1
+    Xentire <- rep(1,n)
+    while(jpt <= j){
+      Xentire <- cbind(Xentire,data2$Protein^jpt)
+      jpt <- jpt + 1
+    }
+    entirepredicted <- Xentire %*% wvalues[[j]]
+    loglik <- n * (log(2 * pi) + log(sigma2)) + 
+      1 / sigma2 * sum((data2$Moisture - entirepredicted)^2)
+    AIC <- loglik + 2 * (j + 2)
+    AICs <- c(AICs , AIC)
+  }
+  return(AICs)
+}
+
+AICvec <- AIC_calc(data2,wvalues,sigma2)
+for(j in 1:6){
+  print(paste("AIC at polynomial degree",j,"is :",AICvec[j]))
+}
+
+fat.lm <- lm(Fat ~.,data = data2[,2:102])
+fat.lm2 <- stepAIC(fat.lm,trace = FALSE)
+fat.lm2$anova
+
+covariates <- scale(data2[,2:101])
+response <- scale(data2[,102])
+ridgemod <- glmnet(as.matrix(covariates), response,
+                   family = "gaussian", alpha = 0)
+plot(ridgemod, xvar = "lambda" , label = TRUE)
+
+lassomod <- glmnet(as.matrix(covariates), response,
+                   family = "gaussian", alpha = 1)
+plot(lassomod, xvar = "lambda" , label = TRUE)
+
+#lambda search for smaller lambda is not very fruitful since the sd of cvm
+#is larger than the improvement.
+lassocv <- cv.glmnet(as.matrix(covariates), response, alpha =1)
+plot(lassocv)
+lassocv$lambda.min
+coef(lassocv, s= "lambda.min")
+# 14 parameters are non-zero, including inercept
